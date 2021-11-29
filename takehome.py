fbfbac48644d734febfeb5f8ede4c8bb30c07493
Trevor Weber <trevaweb@gmail.com>
2021-11-29 15:14:10 -0600

    Implement stringRotation method

--- takehome.py
+++ takehome.py
@@ -13,8 +13,12 @@ class RTSTakeHome:
 
         return aboveBelowDict
 
+    def stringRotation(self, ogString, rotation):
+        #build a string with the end rotation amount prepended to the begining of the original string minus the end rotation amount
+        newString = ogString[-rotation:] + ogString[:-rotation]
+        return newString
+
 if __name__ == '__main__':
     x = RTSTakeHome()
     x.aboveBelow([1, 6, 2, 1, 10], 6)
-
-
+    x.stringRotation("test this string", 5)
